aa5ad51ba4ea3edd4fa2e12b27aa3ed979c41433
Saravanan Gnanaguru <g.gsaravanan@gmail.com>
2018-10-03 23:31:48 +0530

    minor correct in EBS volume encryption script

--- ec2_ebs_encrypt.sh
+++ ec2_ebs_encrypt.sh
@@ -34,68 +34,68 @@ instance_id=$1
 ######### Part 1 section - starts here ##############
 # Stop instances
 echo "Stop instance"
-aws.cmd ec2 stop-instances --instance-ids $instance_id
+aws ec2 stop-instances --instance-ids $instance_id
 
 while [ "$instance_state" != "stopped" ]
 do
-instance_state=`aws.cmd ec2 describe-instances --instance-ids $instance_id | grep -A 3 "State" | grep -i "Name" | awk -F'\"' '{ print  $4 }'`
+instance_state=`aws ec2 describe-instances --instance-ids $instance_id | grep -A 3 "State" | grep -i "Name" | awk -F'\"' '{ print  $4 }'`
 echo "Waiting to instance state changed to STOPPED"
 done
 
 echo "Get Volume ID not encrypted"
-volume=`aws.cmd ec2 describe-instances --filters "Name=instance-id,Values=$instance_id" | grep -A 6 "sda1" | grep -i "VolumeID" | awk -F'\"' '{ print  $4 }'`
+volume=`aws ec2 describe-instances --filters "Name=instance-id,Values=$instance_id" | grep -A 6 "sda1" | grep -i "VolumeID" | awk -F'\"' '{ print  $4 }'`
 echo $volume
 
 echo "Create snapshot of Volume"
-snapshot_id=`aws.cmd ec2 create-snapshot --volume-id $volume | grep -i "SnapshotId" | awk -F'\"' '{ print $4 }'`
+snapshot_id=`aws ec2 create-snapshot --volume-id $volume | grep -i "SnapshotId" | awk -F'\"' '{ print $4 }'`
 echo $snapshot_id
 
 while [ "$snapshot1_status" != "completed" ]
 do
-snapshot1_status=`aws.cmd ec2 describe-snapshots --snapshot-ids $snapshot_id | grep -i "State" | awk -F'\"' '{ print  $4 }'`
+snapshot1_status=`aws ec2 describe-snapshots --snapshot-ids $snapshot_id | grep -i "State" | awk -F'\"' '{ print  $4 }'`
 echo "Waiting for Source snapshot creation to be completed"
 done
 
 echo "Copy the snapshot and encrypt it"
-copied_snapshot=`aws.cmd --region us-west-2 ec2 copy-snapshot --source-region us-west-2 --source-snapshot-id $snapshot_id --encrypted  | grep -i "SnapshotId" | awk -F'\"' '{ print  $4 }'`
+copied_snapshot=`aws --region us-west-2 ec2 copy-snapshot --source-region us-west-2 --source-snapshot-id $snapshot_id --encrypted  | grep -i "SnapshotId" | awk -F'\"' '{ print  $4 }'`
 echo $copied_snapshot
 
 while [ "$snapshot2_status" != "completed" ]
 do
-snapshot2_status=`aws.cmd ec2 describe-snapshots --snapshot-ids $copied_snapshot | grep -i "State" | awk -F'\"' '{ print  $4 }'`
+snapshot2_status=`aws ec2 describe-snapshots --snapshot-ids $copied_snapshot | grep -i "State" | awk -F'\"' '{ print  $4 }'`
 echo "Waiting form copied snapshot creation to be completed"
 done
 ######### Part 1 section - ends here ##############
 
 ######### Part 2 section - starts here ##############
 echo "Create new volume from encrypted snapshot"
-new_encrypt_volume=`aws.cmd ec2 create-volume --size 10 --region us-west-2 --availability-zone us-west-2b --volume-type gp2 --snapshot-id $copied_snapshot | grep -i "VolumeID" | awk -F'\"' '{ print  $4 }'`
+new_encrypt_volume=`aws ec2 create-volume --size 10 --region us-west-2 --availability-zone us-west-2b --volume-type gp2 --snapshot-id $copied_snapshot | grep -i "VolumeID" | awk -F'\"' '{ print  $4 }'`
 echo $new_encrypt_volume
 
 while [ "$new_volume_state" != "available" ]
 do
-new_volume_state=`aws.cmd ec2 describe-volumes --volume-ids $new_encrypt_volume | grep -i "State" | awk -F'\"' '{ print  $4 }'`
+new_volume_state=`aws ec2 describe-volumes --volume-ids $new_encrypt_volume | grep -i "State" | awk -F'\"' '{ print  $4 }'`
 echo "Waiting for new volume creation to be completed"
 done
 
 echo "Detach un-encrypted volume"
-aws.cmd ec2 detach-volume --volume-id $volume
+aws ec2 detach-volume --volume-id $volume
 
 while [ "$old_volume_state" != "available" ]
 do
-old_volume_state=`aws.cmd ec2 describe-volumes --volume-ids $volume | grep -i "State" | awk -F'\"' '{ print  $4 }'`
+old_volume_state=`aws ec2 describe-volumes --volume-ids $volume | grep -i "State" | awk -F'\"' '{ print  $4 }'`
 echo "Waiting for new volume creation to be completed"
 done
 
 echo "Attach newly encrypted volume"
-aws.cmd ec2 attach-volume --volume-id $new_encrypt_volume --instance-id $instance_id --device /dev/sda1
+aws ec2 attach-volume --volume-id $new_encrypt_volume --instance-id $instance_id --device /dev/sda1
 
 while [ "$new_volume_state" != "in-use" ]
 do
-old_volume_state=`aws.cmd ec2 describe-volumes --volume-ids $new_encrypt_volume | grep -i "State" | awk -F'\"' '{ print  $4 }'`
+old_volume_state=`aws ec2 describe-volumes --volume-ids $new_encrypt_volume | grep -i "State" | awk -F'\"' '{ print  $4 }'`
 echo "Waiting for attaching new volume to be completed"
 done
 
 echo " Start instances"
-aws.cmd ec2 start-instances --instance-ids $instance_id
-######### Part 2 section - starts here ##############
+aws ec2 start-instances --instance-ids $instance_id
+######### Part 2 section - ends here ##############
